e8b1952e9b0f09bf3da01803d29cc4f212fcdd44
Ares75643 <92365761+Ares75643@users.noreply.github.com>
2025-04-22 15:32:49 +0200

    Serializers

---
+++ Serializers/BINARYSerializer.cs
@@ -0,0 +1,24 @@
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Telemetry {
+    // Serializar en binario
+    public class BinarySerializer : Serializer {
+        public string Serialize(TelemetryEvent t_event) {
+            #pragma warning disable SYSLIB0011
+            var formatter = new BinaryFormatter();
+            #pragma warning restore SYSLIB0011
+
+            // Serializar en memoria
+            using var memoryStream = new MemoryStream();
+            formatter.Serialize(memoryStream, t_event);
+
+            // Convertir los bytes a string usando base64 para evitar caracteres extraños
+            return Convert.ToBase64String(memoryStream.ToArray());
+        }
+
+        public string Extension() {
+            return ".bin";
+        }
+    }
+}
---
+++ Serializers/CSVSerializer.cs
@@ -0,0 +1,28 @@
+using System.Text;
+using System.Reflection;
+using System.Linq;
+
+namespace Telemetry {
+    // Serializar en CSV
+    public class CsvSerializer : Serializer {
+        public string Serialize(TelemetryEvent t_event) {
+            var type = t_event.GetType();
+            var baseType = type.BaseType;
+
+            // Obtener todas las propiedades: primero las del padre, luego las propias
+            var allProperties = baseType.GetProperties()
+                                        .Concat(type.GetProperties()
+                                                    .Where(p => p.DeclaringType == type));
+
+            // Serializar valores 
+            var values = allProperties
+                         .Select(p => p.GetValue(t_event)?.ToString() ?? string.Empty);
+
+            return string.Join(";", values);
+        }
+
+        public string Extension(){
+            return ".csv";
+        }
+    }
+}
